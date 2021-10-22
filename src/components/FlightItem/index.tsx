9eaacdbc7759b49c23df40c0a7d0e4edb99fa3c0
zionjke <vincere802@gmail.com>
2021-10-22 22:15:54 +0300

    Сделал статус полетов, автопагинацию и фильтрацию по времени.

--- src/components/FlightItem/index.tsx
+++ src/components/FlightItem/index.tsx
@@ -10,16 +10,19 @@ type Props = {
 };
 
 export const FlightItem: React.FC<Props> = (props) => {
+
     const {obj} = props
-    const airport = useSelector<RootState,AirportType[]>(state => {
+    const airport = useSelector<RootState, AirportType[]>(state => {
         return state.app.airports.filter(airport => airport.codeIataAirport === obj.departure.iataCode)
     })
-    const city = useSelector<RootState,CityType[]>(state => {
+    const city = useSelector<RootState, CityType[]>(state => {
         return state.app.cities.filter(city => city.codeIataCity === airport[0].codeIataCity)
     })
-    const airline = useSelector<RootState,AirlineType[]>(state => {
+    const airline = useSelector<RootState, AirlineType[]>(state => {
         return state.app.airlines.filter(airline => airline.codeIataAirline === obj.airline.iataCode)
     })
+
+
     return (
         <tr>
             <td>{obj.flight.iataNumber}</td>
@@ -27,7 +30,14 @@ export const FlightItem: React.FC<Props> = (props) => {
             <td>{city.length && city[0].nameCity}</td>
             <td>{airline.length && airline[0].nameAirline}</td>
             <td>{obj.arrival.terminal}</td>
-            <td>{obj.status}</td>
+            <td>
+                {
+                    obj.arrival.estimatedTime !== null
+                        ? `Очiкується о ${moment(obj.arrival.estimatedTime).format('HH:mm')}`
+                        : 'За розкладом'
+
+                }
+            </td>
         </tr>
     );
 };
--- src/components/FlightsList/index.tsx
+++ src/components/FlightsList/index.tsx
@@ -6,17 +6,51 @@ import {RootState} from '../../store';
 import {ArrivalFlightType} from "../../types";
 import {fetchArrivalFlights} from "../../store/appReducer";
 import {FlightItem} from "../FlightItem";
+import moment from "moment";
 
 
 type Props = {};
 
 export const FlightsList: React.FC<Props> = (props) => {
+    const [count, setCount] = React.useState<number>(1)
+    const [page,setPage] = React.useState<boolean>(false)
     const arrivalFlights = useSelector<RootState, ArrivalFlightType[]>(state => state.app.arrivalFlights)
     const dispatch = useDispatch()
 
+    let now = moment().subtract('10', "minutes").format('YYYY-MM-DDTHH:mm:ss.SSS')
+    let filteredFlights = arrivalFlights.filter(item => item.arrival.scheduledTime >= now).slice(0, 36)
+    let FLIGHT_PER_PAGE = 18;
+    let startIndex = (count - 1) * FLIGHT_PER_PAGE
+    let selectedFlights = filteredFlights.slice(startIndex, startIndex + FLIGHT_PER_PAGE)
+
+
     useEffect(() => {
+        const id = setInterval(() => {
+            dispatch(fetchArrivalFlights())
+        }, 300000);
+
         dispatch(fetchArrivalFlights())
-    }, [])
+
+        return () => clearInterval(id);
+    }, [dispatch])
+
+    useEffect(() => {
+
+        const intervalId = setInterval(() => {
+            if (filteredFlights.length > 18) {
+                if (startIndex === FLIGHT_PER_PAGE) {
+                    setCount(1)
+                } else {
+                    setCount(count + 1)
+                }
+            }
+            setPage(!page)
+        }, 30000)
+
+        return () => clearInterval(intervalId)
+
+    }, [page])
+
 
     return (
         <div className={styles.list}>
@@ -33,7 +67,9 @@ export const FlightsList: React.FC<Props> = (props) => {
                     <th>{'Термінал'}</th>
                     <th>{'Статус'}</th>
                 </tr>
-                {arrivalFlights.map(obj => <FlightItem obj={obj}/>)}
+                {
+                    selectedFlights.map((obj, index) => <FlightItem key={index} obj={obj}/>)
+                }
             </table>
         </div>
     );
